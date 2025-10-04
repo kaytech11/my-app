aa6fd7592de1f2282d7c83a3c52ce9f190f31172
Kaytech11 <kayodeomoniyi319@gmail.com>
2025-10-04 23:18:36 +0100

    Use Render backend URL for API

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,7 +1,8 @@
 
 import axios from "axios";
 
-const API = "http://localhost:3000/users";
+const API = `${import.meta.env.VITE_API_URL}/users`;
+
 
 // Sign up a new user
 export const signup = async (user) => {
